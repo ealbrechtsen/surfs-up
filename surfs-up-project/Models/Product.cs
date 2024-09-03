00956ecb360a49964c9b9130a8a1bd0182fef7da
Rasmus Nørby <52028666+r4sNorby@users.noreply.github.com>
2024-09-03 12:41:41 +0200

    Product controller and repository
    Co-Authored-By: SahraAB <146075964+SahraAB@users.noreply.github.com>
    Co-Authored-By: sophiesorensen <145102712+sophiesorensen@users.noreply.github.com>

---
+++ surfs-up-project/Models/Product.cs
@@ -0,0 +1,20 @@
+namespace surfs_up_project.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+
+        public double Width { get; set; }
+
+        public double Thickness { get; set; }
+        public double Volume { get; set; }
+
+        public string Type { get; set; }
+
+        public double Price { get; set; }
+
+    }
+
+}
