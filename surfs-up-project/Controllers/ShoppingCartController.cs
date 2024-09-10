8cf28e70763edcab4fe54433c5bf9517b92cebfb
Daniel Madsen <daniel.s.madsen@gmail.com>
2024-09-10 09:46:11 +0200

    Created ShoppingCart Controller and Index

---
+++ surfs-up-project/Controllers/ShoppingCartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace surfs_up_project.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
