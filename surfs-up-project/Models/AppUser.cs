3364f3e52ce89852057f63f142190a4bd67fa267
ealbrechtsen <emilmalikalbrechtsen@gmail.com>
2024-09-20 10:41:21 +0200

    added nuget and created Data folder. Missing db and connectionstring

---
+++ surfs-up-project/Models/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace surfs_up_project.Models
+{
+    public class AppUser:IdentityUser
+    {
+        [StringLength(100)]
+        [MaxLength(100)]
+        [Required]
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+
+
+    }
+}
--- surfs-up-project/Program.cs
+++ surfs-up-project/Program.cs
@@ -1,5 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.
+
 builder.Services.AddControllersWithViews(); 
 
 var app = builder.Build();
