39d336e2b34dd90f2d0d802a4c8dc5505639fc93
Emil Albrechtsen <145019248+ealbrechtsen@users.noreply.github.com>
2024-10-04 10:25:37 +0200

    Removed weatherforecast api for rental in progress

--- mvc/surfs-up-project/Controllers/RentalController.cs
+++ mvc/surfs-up-project/Controllers/RentalController.cs
@@ -1,14 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using surfs_up_project.Models;
+
 
 namespace surfs_up_project.Controllers
 {
     public class RentalController : Controller
     {
-        public IActionResult Boards()
+        private readonly string apiUrl = "https://localhost:7010/api/"; // 7010 is defined when creating project
+        public async Task<IActionResult> Boards()
         {
-            var boards = ProductRepository.GetProducts();
-            return View(boards);
+            List<Product>? products = [];
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(apiUrl + "rental/boards/"))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string payload = await response.Content.ReadAsStringAsync();
+                        products = JsonConvert.DeserializeObject<List<Product>>(payload);
+                    }
+                    else
+                    {
+                        return BadRequest($"API request failed with status code {response.StatusCode}");
+                    }
+                }
+            }
+            return View(products);
         }
 
         public IActionResult Board(int? id)
@@ -17,16 +35,16 @@ namespace surfs_up_project.Controllers
             return View(board);
         }
 
-        public IActionResult Wetsuit(int? id)
-        {
-            var board = ProductRepository.GetProductById(id);
-            return View(board);
-        }
+        //public IActionResult Wetsuit(int? id)
+        //{
+        //    var board = ProductRepository.GetProductById(id);
+        //    return View(board);
+        //}
 
-        public IActionResult Equipment(int? id)
-        {
-            var board = ProductRepository.GetProductById(id);
-            return View(board);
-        }
+        //public IActionResult Equipment(int? id)
+        //{
+        //    var board = ProductRepository.GetProductById(id);
+        //    return View(board);
+        //}
     }
 }
