bc1a5ef71e3d9291b925ba10856e6606c5c7bcac
Daniel Madsen <daniel.s.madsen@gmail.com>
2025-01-10 14:56:22 +0100

    Added CsvConverter class and ToProducts method

---
+++ mvc/surfs-up-project/Models/CsvConverter.cs
@@ -0,0 +1,28 @@
+﻿namespace surfs_up_project.Models
+{
+    public static class CsvConverter
+    {
+        public static List<Product> ToProducts(string csvPath)
+        {
+            List<Product> products = new List<Product>();
+            string[] lines = csvPath.Split('\n');
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(';');
+                Product product = new Product
+                {
+                    Name = parts[1],
+                    ImagePath = parts[2],
+                    Length = double.Parse(parts[3]),
+                    Width = double.Parse(parts[4]),
+                    Thickness = double.Parse(parts[5]),
+                    Volume = double.Parse(parts[6]),
+                    Type = parts[7],
+                    Price = double.Parse(parts[8])
+                };
+                products.Add(product);
+            }
+            return products;
+        }
+    }
+}
