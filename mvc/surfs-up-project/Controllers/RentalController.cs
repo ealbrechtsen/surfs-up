6652fa161a80ae005153df7e45f138325638199a
Daniel Madsen <daniel.s.madsen@gmail.com>
2025-01-10 16:51:09 +0100

    Finished functionality for RentalController - needs fixing

--- mvc/surfs-up-project/Controllers/RentalController.cs
+++ mvc/surfs-up-project/Controllers/RentalController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using surfs_up_project.Models;
+using System.Text;
 
 
 namespace surfs_up_project.Controllers
@@ -8,14 +9,39 @@ namespace surfs_up_project.Controllers
     public class RentalController : Controller
     {
         private readonly string apiUrl = "https://localhost:7010/api/"; // 7010 is defined when creating project
+
+
         public async Task<IActionResult> Boards()
         {
             // Define empty list of products
             List<Product>? products = [];
 
-            // Create httpClient to handle request
+            // Convert CSV to list of Products
+            List<Product>? productsToPost = CsvConverter.ToProducts(@"C:\Users\Asus\source\repos\GIT_surfs-up\mvc\surfs-up-project\wwwroot\boards.csv");
+
+            // Serialize list of products to JSON
+            string json = JsonConvert.SerializeObject(productsToPost);
+
+            // Create content to send to API
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            // Create HttpClient instance to handle request
             using (var httpClient = new HttpClient())
             {
+                // Call API and await response
+                using (var response = await httpClient.PostAsync(apiUrl + "rental/boards/", content))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // If success, do nothing
+                    }
+                    else
+                    {
+                        // Otherwise return error
+                        return BadRequest($"API request failed with status code {response.StatusCode}");
+                    }
+                }
+
                 // Call API and await response
                 using (var response = await httpClient.GetAsync(apiUrl + "rental/boards/"))
                 {
@@ -32,6 +58,7 @@ namespace surfs_up_project.Controllers
                     }
                 }
             }
+
             return View(products);
         }
 
